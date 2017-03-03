120060622b7c589bc8eea3a59a7f9e23ce8cfe71
Nicolas Trangez <ikke@nicolast.be>
2017-03-04 00:43:20 +0100

    Extend `AWS4AuthView` unit-tests

--- src/ui/aws4-auth-view.test.js
+++ src/ui/aws4-auth-view.test.js
@@ -53,4 +53,55 @@ describe('AWS4AuthView', () => {
     expect(view.$('.input_aws4_keyId_entry').hasClass('error')).toBe(false);
     expect(view.$('.input_aws4_key_entry').hasClass('error')).toBe(false);
   });
+
+  ['keyId', 'key'].forEach((fieldName) => {
+    it(`reacts on change events of the '${fieldName}' input field`, () => {
+      const model = new AWS4AuthModel({
+        'x-service': 'tests',
+      });
+      const view = new AWS4AuthView({ model }).render();
+      const input = view.$(`.input_aws4_${fieldName}_entry`);
+
+      view.highlightInvalid();
+      expect(input.hasClass('error')).toBe(true);
+
+      const value = `the${fieldName.replace(/^k/, 'K')}`;
+
+      input.val(value);
+      /* For some reason, this doesn't get triggered automatically, whilst it
+       * is in a browser
+       */
+      view.inputChange({
+        target: input,
+      });
+
+      expect(input.hasClass('error')).toBe(false);
+      expect(model.get(fieldName)).toBe(value);
+    });
+  });
+
+  it('doesn\'t highlight valid fields', () => {
+    const view = new AWS4AuthView({
+      model: new AWS4AuthModel({
+        'x-service': 'tests',
+      }),
+    }).render();
+    const keyIdField = view.$('.input_aws4_keyId_entry');
+    const keyField = view.$('.input_aws4_key_entry');
+
+    keyIdField.val('theKeyId');
+    keyField.val('theKey');
+
+    view.inputChange({
+      target: keyIdField,
+    });
+    view.inputChange({
+      target: keyField,
+    });
+
+    view.highlightInvalid();
+
+    expect(keyIdField.hasClass('error')).toBe(false);
+    expect(keyField.hasClass('error')).toBe(false);
+  });
 });
