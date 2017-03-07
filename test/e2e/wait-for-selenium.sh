409b73fed0f759c08ab8145bf4e21fedde4891dc
Nicolas Trangez <ikke@nicolast.be>
2017-03-07 13:41:51 +0100

    E2E: Disable Firefox testing
    It looks like the Firefox Selenium driver (`geckodriver`) has some major
    issues with event triggering, causing tests to fail quite randomly. More
    exactly, the `change` event on `input` elements isn't triggered even
    when focus changes. Furthermore, adding some 'strategic sleeps' in the
    test code doesn't really help.
    
    This could be related to @mozilla/geckodriver#348. Not sure.
    
    In order for CI and manual tests not to fail randomly, disabling the
    Firefox-based tests for now. Should be easy to revert at some point, and
    E2E tests working in Chrome should give a reasonable level of
    confidence, since the app doesn't really use any brand new browser
    features.
    
    See: https://github.com/mozilla/geckodriver/issues/348

--- test/e2e/wait-for-selenium.sh
+++ test/e2e/wait-for-selenium.sh
@@ -3,32 +3,14 @@
 HOST=localhost
 PORT=4444
 
-ADDRESS="http://${HOST}:${PORT}/grid/console"
+ADDRESS="http://${HOST}:${PORT}/wd/hub/static/resource/hub.html"
 
 probeSelenium() {
     curl -s ${ADDRESS} > /dev/null
     return $?
 }
 
-probeBrowser() {
-    local browser
-    browser=$1
-
-    curl -s ${ADDRESS} | grep "browserName=${browser}" > /dev/null
-    return $?
-}
-
 until probeSelenium; do
     >&2 echo "Selenium unvailable. Sleeping..."
-    sleep 1
-done
-
-until probeBrowser 'chrome'; do
-    >&2 echo "Chrome node unavailable. Sleeping..."
-    sleep 1
-done
-
-until probeBrowser 'firefox'; do
-    >&2 echo "Firefox not unavailable. Sleeping..."
     sleep 1
 done
